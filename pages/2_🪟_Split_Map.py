3c505098ad115f719d66c7689a34cc60528aeacc
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-11-30 22:34:45 +0800

    Update 2_🪟_Split_Map.py
    fix indentation error from chatgpt code

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -35,20 +35,49 @@ with st.expander("See source code"):
             left_colormap="RdYlGn",  # Apply the 'RdYlGn' colormap for the left layer
             right_colormap="RdYlGn"  # Apply the 'RdYlGn' colormap for the right layer
         )
-        #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-
+        
         # Add the custom legend to the left side of the map
-         legend_html = """
-            <div style="position: fixed; 
-                        bottom: 10px; right: 10px; width: 200px; height: 150px; 
-                        border:2px solid grey; background-color:white; 
-                        z-index:9999; font-size:14px; padding: 10px;">
-            <b>NDVI Legend</b><br>
-            <img src="https://www.colorhexa.com/000000.png" width="30" height="30"> Low NDVI (Black)<br>
-            <img src="https://www.colorhexa.com/bfbfbf.png" width="30" height="30"> High NDVI (Light Gray)<br>
-            </div>
-            """
+        legend_html = """
+        <div style="position: fixed; 
+                    bottom: 10px; right: 10px; width: 200px; height: 150px; 
+                    border:2px solid grey; background-color:white; 
+                    z-index:9999; font-size:14px; padding: 10px;">
+        <b>NDVI Legend</b><br>
+        <img src="https://www.colorhexa.com/000000.png" width="30" height="30"> Low NDVI (Black)<br>
+        <img src="https://www.colorhexa.com/bfbfbf.png" width="30" height="30"> High NDVI (Light Gray)<br>
+        </div>
+        """
         m.get_root().html.add_child(folium.Element(legend_html))
 
+        m.to_streamlit(height=700)
 
-m.to_streamlit(height=700)
+
+
+
+# with st.expander("See source code"):
+#     with st.echo():
+#         m = leafmap.Map()
+#         m.split_map(
+#             #left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+#             left_layer="https://chinchillaz.github.io/streamlit-hw/ndvi_20240901_field.tif", 
+#             right_layer="https://chinchillaz.github.io/streamlit-hw/ndvi_20240911_field.tif",
+#             left_colormap="RdYlGn",  # Apply the 'RdYlGn' colormap for the left layer
+#             right_colormap="RdYlGn"  # Apply the 'RdYlGn' colormap for the right layer
+#         )
+#         #m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+
+#         # Add the custom legend to the left side of the map
+#          legend_html = """
+#             <div style="position: fixed; 
+#                         bottom: 10px; right: 10px; width: 200px; height: 150px; 
+#                         border:2px solid grey; background-color:white; 
+#                         z-index:9999; font-size:14px; padding: 10px;">
+#             <b>NDVI Legend</b><br>
+#             <img src="https://www.colorhexa.com/000000.png" width="30" height="30"> Low NDVI (Black)<br>
+#             <img src="https://www.colorhexa.com/bfbfbf.png" width="30" height="30"> High NDVI (Light Gray)<br>
+#             </div>
+#             """
+#         m.get_root().html.add_child(folium.Element(legend_html))
+
+
+# m.to_streamlit(height=700)
